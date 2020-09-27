90a6eb4df78881c9b7d1c8a80eff6ef96a7eb191
Donald <dcatoii@gmail.com>
2020-09-27 15:47:31 -0700

    Playerdata now uses actually playerprefs for unlocking parts

--- Assets/Scripts/Data/PlayerData.cs
+++ Assets/Scripts/Data/PlayerData.cs
@@ -8,7 +8,11 @@ public class PlayerData {
         set { PlayerPrefs.SetInt("PlayerCurrency", value); }
     }
 
-    public static bool IsPartUnlocked(string partID) { return false; }
+    public static bool IsPartUnlocked(string partID) { return PlayerPrefs.GetInt(partID + "_unlocked", 0) == 1; }
+    public static void UnlockPart(string partID) { PlayerPrefs.SetInt(partID + "_unlocked", 1); }
+
+    public static int GetPartUpgradesPurchased(string partID) { return PlayerPrefs.GetInt(partID + "_level", 0); }
+    public static void PartUpgadePurchased(string partID) { PlayerPrefs.SetInt(partID + "_level", PlayerPrefs.GetInt(partID + "_level", 0)); }
 
     public static string CustomMechData
     {
