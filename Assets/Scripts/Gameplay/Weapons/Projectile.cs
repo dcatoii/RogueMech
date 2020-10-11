d45da1d5ff47b44920865aeef3143967a4f0fae1
Donald <dcatoii@gmail.com>
2020-10-11 14:42:17 -0700

    Adjusted the terrain and projectiles to fix tunneling bugs + level flow
    Build 0.3.0.21

--- Assets/Scripts/Gameplay/Weapons/Projectile.cs
+++ Assets/Scripts/Gameplay/Weapons/Projectile.cs
@@ -8,9 +8,10 @@ public class Projectile : MonoBehaviour {
     public GameObject deathEffect;
     public Mob Source;
     public int Damage = 800;
+    Vector3 lastPos;
 	// Use this for initialization
 	void Start () {
-		
+        lastPos = transform.position;
 	}
 	
     public virtual void SetDamage(int value)
@@ -20,9 +21,33 @@ public class Projectile : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
-        transform.position += transform.forward * Time.fixedDeltaTime * speed;	
 
-	}
+        // Calculate the desired movement vector
+        Vector3 move = transform.forward * Time.fixedDeltaTime * speed;
+
+        // Store the result of the collider cast
+        RaycastHit hit;
+
+        // Check continuously until the collision is resolved
+        if (Physics.SphereCast(transform.position, 0.3f,
+                                 move.normalized, out hit, move.magnitude, LayerMask.NameToLayer("Terrain")))
+        {
+            Object.Destroy(this.gameObject);
+            GameObject.Instantiate(deathEffect, transform.position, Quaternion.identity);
+        }
+
+        transform.position += move;
+        
+        /*
+        //check spherecast to prevent terriain tunneling
+        RaycastHit info = new RaycastHit();
+        if (Physics.SphereCast(lastPos, 0.1f, (transform.position - lastPos).normalized, out info, (transform.position - lastPos).magnitude, LayerMask.NameToLayer("Terrain")))
+        {
+               Object.Destroy(this.gameObject);
+                GameObject.Instantiate(deathEffect, transform.position, Quaternion.identity);
+        }
+        lastPos = transform.position;*/
+    }
 
     public void SetTarget(Vector3 target)
     {
@@ -42,12 +67,13 @@ public class Projectile : MonoBehaviour {
                 return;
 
             MechComponentCollisionDetector frameCollision = collision.collider.GetComponent<MechComponentCollisionDetector>();
-            if(frameCollision != null) //we collided with a mech
+            if (frameCollision != null) //we collided with a mech
             {
                 frameCollision.component.OnHit(this);
             }
             Object.Destroy(this.gameObject);
             GameObject.Instantiate(deathEffect, transform.position, Quaternion.identity);
-        }
+        }       
+            
     }
 }
