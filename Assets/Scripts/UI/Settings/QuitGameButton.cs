2838da67c355060dba381e753b538b9a34ebb4b7
Donald <dcatoii@gmail.com>
2020-10-14 13:14:18 -0700

    Fixed exit button not working in editor

--- Assets/Scripts/UI/Settings/QuitGameButton.cs
+++ Assets/Scripts/UI/Settings/QuitGameButton.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class QuitGameButton : MonoBehaviour {
 
 	public void OnPress(bool showConfirmation)
-    {
-        Application.Quit();
+    { 
+        // save any game data here
+        #if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+         Application.Quit();
+        #endif
     }
 }
